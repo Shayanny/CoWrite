7027e1ab5886e299d4069de3b87c6cc695f0b6b2
Shayanny <shayanny4@outlook.com>
2025-11-15 23:35:58 +0000

    feat: implement JWT authentication middleware

--- api/main.go
+++ api/main.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
 
 	"minidocs/api/config"
 	"minidocs/api/handlers"
+	"minidocs/api/middleware"
+	"minidocs/api/utils"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -31,6 +34,23 @@ func main() {
 	// Authentication routes
 	router.HandleFunc("/api/register", handlers.Register).Methods("POST")
 	router.HandleFunc("/api/login", handlers.Login).Methods("POST")
+
+	router.Handle("/api/protected", middleware.AuthMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get user info from context
+		claims := r.Context().Value(middleware.UserContextKey).(*utils.Claims)
+		
+		// Send back user info as JSON
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"message": "You are authenticated!",
+			"user": map[string]interface{}{
+				"id":       claims.UserID,
+				"username": claims.Username,
+				"email":    claims.Email,
+			},
+		})
+	}))).Methods("GET")
 
 	// Get port from environment or use default
 	port := os.Getenv("PORT")
